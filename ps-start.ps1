fc6a6380eed6363ecf2b388c709d55d0ee91d5d1
jonathan berent <jonathan.berent@dx-fx.com>
2018-11-14 22:39:34 -0500

    add Add-Remote and add remote

--- ps-start.ps1
+++ ps-start.ps1
@@ -32,3 +32,54 @@ if (Test-Path "ps-start-$env:USERNAME.ps1") {
 
 #endregion
 
+function RebuildEnvironment {
+    setting vsts  https://dx-fx.visualstudio.com/DefaultCollection/Home/_git/PSRoguelike
+
+}
+
+Add-Help WEB "web" "Get-Web" "e.g., 'web github', 'web docs', 'web issues'"
+function Get-Web { 
+	if ($args.Count -eq 0) {
+        (setting).GetEnumerator() | 
+            Where-Object {$_.value.tostring().startsWith("http")} | 
+            ForEach-Object {$_.Name}
+	} else {
+		Start-Process (settings $args[0])
+	}	
+}
+
+Add-Help GIT "" "Add-Origin -url -name" "create 'origin' remote, e.g., 'Add-Origin github https://github.com/user/project.git'"
+function Add-Origin($name, $url) { Add-Remote "origin" $url $name}
+#function Add-Github($url) { Add-Remote "github" $url "github"}
+Add-Help GIT "" "Add-Remote -remote -url -name" "e.g., 'Add-Remote origin github https://github.com/user/project.git'"
+function Add-Remote($remote, $url, $name) {
+    if (!$remote) {
+        $remote = Read-Host "What is the remote? E.g., origin, upstream, etc. [origin] is the default"
+    }
+    if (!$name) {
+        $name = Read-Host "[OPTIONAL] What is the friendly name of the repository provider? E.g., GitHub, VSTS, etc."
+        if ($name){
+            $msg = " in $name"
+        }
+    }
+    if (!$url) {
+        "Create an empty repo$msg and then enter the url (e.g. https://github.com/user/project.git)" | WARNING
+        $url = Read-Host "Repo URL"
+    }   
+    if ($url){
+        if (!$remote) {
+            $remote = "origin"
+        }
+        #Set the new remote
+        git remote add $remote $url
+        # Verify the new remote URL
+        if (git remote -v | Where-Object {$_.contains($url)}){
+            "Success! Type 'push' to upload your commits" | GOOD
+        }
+        if ($name) {
+            $page = $url.Replace(".git","")
+            setting $name $page
+            "type: 'web $name' to go to: $page" | GOOD
+        }     
+    }
+}
